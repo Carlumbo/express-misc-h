1cd36dc42ab90ef3b4649dd73e1d3d30685404a7
Carlumbo <Carlpalumbo1@gmail.com>
2020-09-28 15:08:40 -0400

    added mongodb connection

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+var mongoose = require("mongoose");
+var mongoDB =
+  "mongodb+srv://superadmin:120622@cluster0.8lc0y.mongodb.net/express-msic?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
