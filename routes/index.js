561d4f0489f2ef1289186139727ba2c5f46b6452
Carlumbo <Carlpalumbo1@gmail.com>
2020-09-28 14:43:34 -0400

    added route for read

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Special = require("../models/special");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// ROUTE FOR READ
+router.get("/", (req, res) => {
+  Special.find((err, docs) => {
+    if (err) throw err;
+    // console.log(docs);
+    res.render("home", {
+      teams: docs,
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
 });
-
 module.exports = router;
