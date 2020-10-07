a8794290f8876f594f2e5c7a5d0b1e300c15aac2
Carlumbo <Carlpalumbo1@gmail.com>
2020-10-07 17:40:27 -0400

    heroku update #2

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,17 @@ var DataController = require("../controllers/DataController");
 // router.get("/", function (req, res, next) {
 //   res.render("data");
 // });
+let logged_in = false;
 
 router.post("/new", DataController.create);
 //list of users
-router.get("/", DataController.read);
+if (logged_in) {
+  router.get("/", DataController.read);
+} else {
+  router.get("/", function (req, res, next) {
+    res.send("Please Login");
+  });
+}
 //updates
 router.post("/edit/:id", DataController.update);
 // delete
